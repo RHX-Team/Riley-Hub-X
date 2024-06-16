c12f762400cdf1d653765b9611b262b08d53540d
RileyBeeRBLX <rileybeerblx3@gmail.com>
2024-06-16 18:53:59 +0800

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -2,7 +2,7 @@ getgenv().Games = {
   [2753915549] = "https://raw.githubusercontent.com/RileyBeeOrion/Riley-Hub-X/main/Games/RileyHubXBloxFruit.lua", -- Blox Fruit Sea 1
   [4442272183] = "https://raw.githubusercontent.com/RileyBeeOrion/Riley-Hub-X/main/Games/RileyHubXBloxFruit.lua", -- Blox Fruit Sea 2
   [7449423635] = "https://raw.githubusercontent.com/RileyBeeOrion/Riley-Hub-X/main/Games/RileyHubXBloxFruit.lua", -- Blox Fruit Sea 3
-  [286090429] = "https://raw.githubusercontent.com/RileyBeeOrion/Riley-Hub-X/main/Games/RileyHubXArsenalPC.lua", -- Arsenal (PC)
+  [13772394625] = "https://raw.githubusercontent.com/RHX-Team/Riley-Hub-X/main/Games/RileyHubXBladeBall.lua", -- Blade Ball
   [286090429] = "https://raw.githubusercontent.com/RileyBeeOrion/Riley-Hub-X/main/Games/RileyHubXArsenal.lua", -- Arsenal (Mobile)
-  [13864667823] = "https://raw.githubusercontent.com/RileyBeeOrion/Riley-Hub-X/main/Games/RileyHubXBreakIn2.lua",  -- Break In 2 Lobby / Game
+  [13864667823] = "https://raw.githubusercontent.com/RileyBeeOrion/Riley-Hub-X/main/Games/RileyHubXBreakIn2.lua",  -- Break In 2 Game
 }
