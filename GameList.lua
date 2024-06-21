39f87632bdc8fe1a027f5c3d8df98b12c80ffbf6
RileyBeeRBLX <rileybeerblx3@gmail.com>
2024-06-21 21:09:52 +0800

    Delete GameList.lua

--- GameList.lua
+++
@@ -1,9 +0,0 @@
-getgenv().Games = {
-  [2753915549] = "https://raw.githubusercontent.com/RileyBeeOrion/Riley-Hub-X/main/Games/RileyHubXBloxFruit.lua", -- Blox Fruit Sea 1
-  [4442272183] = "https://raw.githubusercontent.com/RileyBeeOrion/Riley-Hub-X/main/Games/RileyHubXBloxFruit.lua", -- Blox Fruit Sea 2
-  [7449423635] = "https://raw.githubusercontent.com/RileyBeeOrion/Riley-Hub-X/main/Games/RileyHubXBloxFruit.lua", -- Blox Fruit Sea 3
-  [13772394625] = "https://raw.githubusercontent.com/RHX-Team/Riley-Hub-X/main/Games/RileyHubXBladeBall.lua", -- Blade Ball
-  [6516141723] = "https://raw.githubusercontent.com/RHX-Team/Riley-Hub-X/main/Games/RileyHubXDoors", -- Doors
-  [286090429] = "https://raw.githubusercontent.com/RileyBeeOrion/Riley-Hub-X/main/Games/RileyHubXArsenal.lua", -- Arsenal (Mobile)
-  [13864667823] = "https://raw.githubusercontent.com/RileyBeeOrion/Riley-Hub-X/main/Games/RileyHubXBreakIn2.lua",  -- Break In 2 Game
-}
