dfbbb53d8263f3a88dd2e11786f6f9281ef3b032
RileyBeeRBLX <rileybeerblx3@gmail.com>
2024-06-21 23:54:10 +0800

    Create GameList.lua

---
+++ GameList.lua
@@ -0,0 +1,11 @@
+getgenv().Games = {
+  [2753915549] = "https://raw.githubusercontent.com/RHX-Team/Riley-Hub-X/main/Games/RileyHubXBloxFruit.lua", -- Blox Fruit Sea 1
+  [4442272183] = "https://raw.githubusercontent.com/RHX-Team/Riley-Hub-X/main/Games/RileyHubXBloxFruit.lua", -- Blox Fruit Sea 2
+  [7449423635] = "https://raw.githubusercontent.com/RHX-Team/Riley-Hub-X/main/Games/RileyHubXBloxFruit.lua", -- Blox Fruit Sea 3
+  [286090429] = "https://raw.githubusercontent.com/RHX-Team/Riley-Hub-X/main/Games/RileyHubXArsenal.lua", -- Arsenal
+  [13772394625] = "https://raw.githubusercontent.com/RHX-Team/Riley-Hub-X/main/Games/RileyHubXBladeBall.lua",  -- Blade Ball
+  [13864661000] = "https://raw.githubusercontent.com/RHX-Team/Riley-Hub-X/main/Games/RileyHubXBreakIn2.lua",  -- Break In 2
+  [6516141723] = "https://raw.githubusercontent.com/RHX-Team/Riley-Hub-X/main/Games/RileyHubXDoors",  -- Doors
+  [142823291] = "https://raw.githubusercontent.com/RHX-Team/Riley-Hub-X/main/Games/RileyHubXMurderMystery2.lua",  -- Murder Mystery 2
+  [189707] = "https://raw.githubusercontent.com/RHX-Team/Riley-Hub-X/main/Games/RileyHubXNaturalDisasterSurvival.lua",  -- Natural Disaster Survival
+}
