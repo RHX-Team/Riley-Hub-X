5b6bec08ed3902813caca49ae9671d4590fde3b2
RileyBeeRBLX <rileybeerblx3@gmail.com>
2024-06-21 23:54:31 +0800

    Update LoaderUpdated.lua

--- LoaderUpdated.lua
+++ LoaderUpdated.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet(""))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RHX-Team/Riley-Hub-X/main/GameList.lua"))()
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
         loadstring(game:HttpGet(Execute))()
